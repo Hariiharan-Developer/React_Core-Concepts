f01bfea5b4381dd65943645f92b1c9f5f031d876
Hariharan.s <hreehs1428@gmail.com>
2025-09-20 15:44:15 +0530

    task 2 completed

--- src/components/9.taskForArray&Obj/TaskHobbie.jsx
+++ src/components/9.taskForArray&Obj/TaskHobbie.jsx
@@ -1,27 +1,37 @@
 import React, { useState } from 'react'
 
 const TaskHobbie = () => {
+    const [count,setCount] = useState(1)
     const [bio,setBio]=useState(
         {
             name:'hari',
             age:25,
             collage:'srmv vas',
             course:'full stack',
-            hobbie:['tv', 'reading', 'games' ,'travel']
+            hobbie:['tv', 'reading', 'games' ,'travel'],
+            hobbie2 :[]
         }
     )
 
-    const hobbieInput =()=>{
-        
-    }
+   const addHobbies =()=>{
+    let name =`hobbie ${count}`
+   
+    setBio((prev) => ({
+  ...prev,
+  hobbie2: [...prev.hobbie2, name]
+}));
 
-    const add = ()=>{
+    setCount((prev)=> {
+        return prev +1
+    })
+   }
 
-    }
+    
   return (
     <div>
       <h1>Task -1</h1>
       <div className="container">
+        <h4>displaying array field inside the object</h4>
         <div className="content">
             <h3>Bio</h3>
             <p>name : {bio.name}</p>
@@ -34,6 +44,26 @@ const TaskHobbie = () => {
         
             })}</p>
 
+        </div>
+      </div>
+      <hr/>
+
+<h1>task -2</h1>
+
+      <div className="container">
+        <h4>add hobbies when clicking the buton</h4>
+        <div className="content">
+            <h3>Bio</h3>
+            <p>name : {bio.name}</p>
+            <p>age : {bio.age}</p>
+            <p>collage : {bio.collage}</p>
+            <p>course : {bio.course}</p>
+            <button onClick={addHobbies}>Add Hobbies</button>
+            <p>hobbies: {
+                bio.hobbie2.map((prev,index)=>{
+                    return <li key={index}>{prev}</li>
+                })
+                }</p>
         </div>
       </div>
     </div>
